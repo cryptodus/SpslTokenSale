c678c3707c3aca486c48e01b81f8a329d423fbba
vbcca <vybccal@gmail.com>
2018-04-28 10:57:41 +0300

    Make contract finalizable

--- contracts/TokenCrowdsale.sol
+++ contracts/TokenCrowdsale.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.4.23;
 
-import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/distribution/FinalizableCrowdsale.sol";
 import './Token.sol';
 
-contract TokenCrowdsale is Crowdsale {
+contract TokenCrowdsale is FinalizableCrowdsale {
   using SafeMath for uint256;
 
   function TokenCrowdsale(Token _token, address _wallet) public
-   Crowdsale(1, _wallet, _token){
+   Crowdsale(1, _wallet, _token)
+   TimedCrowdsale(block.timestamp+1, block.timestamp+2){
 
   }
 }
