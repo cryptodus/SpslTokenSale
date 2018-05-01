85639ecf736c3009a81445cb750728ca06e50000
vbcca <vybccal@gmail.com>
2018-05-01 19:02:22 +0300

    Add tests for finalization

--- contracts/TokenCrowdsale.sol
+++ contracts/TokenCrowdsale.sol
@@ -6,9 +6,19 @@ import './Token.sol';
 contract TokenCrowdsale is FinalizableCrowdsale {
   using SafeMath for uint256;
 
-  function TokenCrowdsale(Token _token, address _wallet) public
+  function TokenCrowdsale(
+      Token _token,
+      address _wallet,
+      uint256 _uncappedRate,
+      uint256[] _rates,
+      uint256[] _capsTo,
+      uint256 _openingTime,
+      uint256 _closingTime,
+      uint256 _uncappedOpeningTime,
+      uint256 _icoTotalCap
+  ) public
    Crowdsale(1, _wallet, _token)
-   TimedCrowdsale(block.timestamp+1, block.timestamp+2){
+   TimedCrowdsale(_openingTime, _closingTime) {
 
   }
 }
